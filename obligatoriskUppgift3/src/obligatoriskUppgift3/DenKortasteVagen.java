6451fd549658439c090db86b16434ea590511e0a
Samuel Ferrara <55464329+ferrara-dev@users.noreply.github.com>
2019-11-12 15:26:00 +0100

    Add files via upload

---
+++ obligatoriskUppgift3/src/obligatoriskUppgift3/DenKortasteVagen.java
@@ -0,0 +1,9 @@
+package obligatoriskUppgift3;
+
+public class DenKortasteVagen 
+{
+	public static void main(String[] args) 
+	{
+		
+	}
+}
---
+++ obligatoriskUppgift3/src/obligatoriskUppgift3/InstansAvKortasteVagen.java
@@ -0,0 +1,39 @@
+package obligatoriskUppgift3;
+
+public class InstansAvKortasteVagen 
+{
+/**
+ [1] Bestäm en instans av det här problemet i fallet att m = 3 och n = 4 – välj väglängderna. Specificera den instansen med en
+	 bild. Det ska framgå vilka stationer och vägar som finns, och hur långa vägarna är. 
+  m = 3
+  n = 4
+ */
+/************************************************[2]*********************************************************************
+ Specificera den valda instansen även med en tabell. Lös den aktuella instansen av problemet med papper och penna:
+     undersök alla möjliga vägar och bestäm mellanstationerna för den kortaste vägen 
+     (för in längderna i kolumnen ”Längd” och välj de mellanstationer som motsvarar den minsta längden).
+	 m = 3
+	 n = 4
+	 
+	  z1 | ai | z2 | bij | z3 | cj | z4 | Längd | Kortaste sträckan |
+	 ----|----|----|-----|----|----|----|-------|-------------------|
+	  x  |  5 | u1 |   3 | v1 | 34 |  Y |    42 |					|
+	  x  |    | u1 |   5 | v2 |  9 |  Y |    19 |	  ----X----	    |
+	  x  |    | u1 |  11 | v3 | 22 |  Y |    38 |					|
+	  x  |    | u1 |   2 | v4 | 31 |  Y |    38 |					|
+	  x  |  9 | u2 |   3 | v1 | 11 |  Y | 	 23 |					|
+	  x  |    | u2 |  33 | v2 | 22 |  Y | 	 64 |					|
+	  x  |    | u2 |   1 | v3 | 33 |  Y | 	 43 |					|
+	  x  |    | u2 |  20 | v4 |  1 |  Y | 	 30 |					|
+	  x  |  4 | u3 |  11 | v1 | 22 |  Y | 	 37 |					|
+	  x  |    | u3 |  13 | v2 |  4 |  Y | 	 21 |					|
+	  x  |    | u3 |  5  | v3 | 27 |  Y | 	 36 |					|
+	  x  |    | u3 |  21 | v4 |  2 |  Y | 	 27 |					|
+	  
+	  
+ *************************************************************************************************************************/
+	
+/************************************************[3]************************************************************************
+ 
+ */
+}
