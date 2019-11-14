300484f70abe1fc0596fd5e252cc60fb04d1ea74
Samuel Ferrara <55464329+ferrara-dev@users.noreply.github.com>
2019-11-14 14:22:10 +0100

    Update HamtaKortasteVagen.java

--- obligatoriskUppgift3/src/obligatoriskUppgift3/HamtaKortasteVagen.java
+++ obligatoriskUppgift3/src/obligatoriskUppgift3/HamtaKortasteVagen.java
@@ -4,51 +4,50 @@ import java.util.*;
 public class HamtaKortasteVagen
 {
 	static Scanner in = new Scanner(System.in);
-	static TheShortestDistance längd = new TheShortestDistance ();
-	static double a[];
-	static double b[][];
-	static double c[];
-	static double min;
-	static int m;
-	static int n;
 	
 	public static void main(String[] args) 
 	{
 		
-		int n = 4;
-		int m = 3;
-		double [] a = new double[m];
-		double [][] b = new double[m][n];	
-		double [] c = new double[n];
-				 
-		c [0] = 34;
-		c [1] = 11;
-		c [2] = 22;
-		c [3] = 22;
-				
-		for(int i = 0; i < m; i++)
+		System.out.println("Skriv in 1 för att testa den första instansen eller 2 för att testa den andra");
+		Scanner in = new Scanner(System.in);
+		int val = in.nextInt();
+
+		/*******FÖRSTA INSTANSEN********
+		* +--------------------------+ *
+		* |    3 stationer i zon 2   | *
+		* |    4 stationer i zon 3   | *
+		* +--------------------------+ *
+		********************************/
+		if(val == 1)
 		{
-			System.out.println("Ange sträcka mellan x och u" + (i+1));
-			a[i] = in.nextDouble();
-						
-			for(int j = 0; j < n; j++)
-			{	
-				System.out.println("Ange sträckan mellan U"+(i+1) +" och V" + (j+1) + " : " );
-				b[i][j] = in.nextDouble();
-			}
-		}	
-				
-		for(int j = 0; j < n; j++)
-		{
-		System.out.println("Ange sträckan mellan V"+(j+1) +" och Y" + (j+1) + " : ");
-		c[j] = in.nextDouble();
-		}
-		// Utför beräkning 
+		double a []   = new double [] {0,10,10,5 };
+		double b [][] = new double [][] {{0,0,0,0,0} ,{0,10,7,7,5},{0,2,12,15,7},{0,10,3,5,15} };
+		double c []   = new double [] {0,2,3,10,5 };
 		double minimum = TheShortestDistance.langd(a,b,c);
 		int bWay [] = TheShortestDistance.mellanstationer (a,b,c);
-		System.out.println("Den kortaste sträckan mellan station X och Y är : " + Arrays.toString(bWay));
+		System.out.println("Den kortaste sträckan går mellan station : " + "u" + (bWay[0]) +" och v" + (bWay[1]));
 		System.out.println("Den kortaste sträckan mellan station X och Y är : " + minimum);
+		}
+		
+		/********ANDRA INSTANSEN********
+		* +--------------------------+ *
+		* |    2 stationer i zon 2   | *
+		* |    5 stationer i zon 3   | *
+		* +--------------------------+ *
+		********************************/
+		if(val == 2)
+		{
+		double a []   = new double [] {0,2,3};
+		double b [][] = new double [][] { {0,0,0,0,0},{0,2,12,15,7,1},{0,10,3,5,15,8} };
+		double c []   = new double [] {0,7,10,5,3,5 };
+		double minimum = TheShortestDistance.langd(a,b,c);
+		int bWay [] = TheShortestDistance.mellanstationer (a,b,c);
+		System.out.println("Den kortaste sträckan går mellan station : " + "u" + (bWay[0]) +" och v" + (bWay[1]));
 		System.out.println("Den kortaste sträckan mellan station X och Y är : " + minimum);
-	}
+
+		}
+
+		in.close();
+	  }
 }
 			
