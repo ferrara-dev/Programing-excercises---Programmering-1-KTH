3d21c4e78029b8199ccae4775afae72d56a0d98a
Samuel Ferrara <55464329+ferrara-dev@users.noreply.github.com>
2019-11-18 09:21:44 +0100

    Update OperationerMedStrangarLatt.java

--- obligatoriskUppgift4/src/obligatoriskUppgift4/OperationerMedStrangarLatt.java
+++ obligatoriskUppgift4/src/obligatoriskUppgift4/OperationerMedStrangarLatt.java
@@ -1,5 +1,50 @@
+/**Klass som används för beräkningar av tal representerade som strängar**
+ 
+ 	Author: Samuel Ferara
+
+ 	Beskrivning: 
+ 	Skapar objekt av klassen BigDecimal och kopierar tal som finns i strängar till dessa. 
+ 	Utför diverse beräkningar genom metoder som finns i klassen BigDecimal för att sedan 
+ 	omvandla det mottade resultatet till sträng-format och returnera det.
+ 	
+ ************************************************************************************/
+
+
 package obligatoriskUppgift4;
 
-public class EasyOpMS {
+import java.math.BigDecimal;
 
+public class OperationerMedStrangarLatt 
+{
+	public static String addString(String a, String b) 
+	{
+		BigDecimal numA = new BigDecimal(a);
+		BigDecimal numB = new BigDecimal(b);
+		
+		return numA.add(numB).toString();
+	}
+	
+	public static String multiplyString(String a, String b)
+	{
+		BigDecimal numA = new BigDecimal(a);
+		BigDecimal numB = new BigDecimal(b);
+		
+		return numA.multiply(numB).toString(); 
+	}
+	
+	public static String divideString(String a, String b)
+	{
+		BigDecimal numA = new BigDecimal(a);
+		BigDecimal numB = new BigDecimal(b);
+		
+		return numA.divide(numB).toString(); 
+	}
+	
+	public static String subtractString(String a, String b)
+	{
+		BigDecimal numA = new BigDecimal(a);
+		BigDecimal numB = new BigDecimal(b);
+		
+		return numA.subtract(numB).toString(); 
+	}
 }
