16fe5f777abb8cdee55ad95e223b8e40f82efc12
Samuel Ferrara <55464329+ferrara-dev@users.noreply.github.com>
2019-12-11 16:48:59 +0100

    Update PolyLineTest.java

--- eu4/src/eu4/PolyLineTest.java
+++ eu4/src/eu4/PolyLineTest.java
@@ -3,6 +3,9 @@ package eu4;
 import java.io.PrintWriter;
 import java.util.Iterator;
 
+
+
+
 public class PolyLineTest 
 {
 	public static void main(String[] args) 
@@ -13,10 +16,15 @@ public class PolyLineTest
 	        Punkt p1 = new Punkt("A", 3, 4);
 	        Punkt p2 = new Punkt("B", 5, 6);
 	        Punkt p3 = new Punkt("C", 1, 7);
+	        
+	        Punkt p11 = new Punkt("A", 8, 3);
+	        Punkt p22 = new Punkt("B", 9, 9);
+	        Punkt p33 = new Punkt("C", 1, 19);
 
-	        Punkt[] PunktLista = {p1, p2, p3};
-	        NPolylinje nLine = new NPolylinje(PunktLista);
-	        VPolylinje vLine = new VPolylinje(PunktLista);
+	        Punkt[] PunktLista1 = {p1, p2, p3};
+	        Punkt[] PunktLista2 = {p11, p22, p33};
+	        NPolylinje nLine = new NPolylinje(PunktLista1);
+	        VPolylinje vLine = new VPolylinje(PunktLista2);
 
 	        Punkt p4 = new Punkt("X", 4, 1);
 	        Punkt p5 = new Punkt("Y", 3, 2);
@@ -52,16 +60,18 @@ public class PolyLineTest
 
 	        // iterator
 	        int count = 1;
-	        out.println("\nIterator");
+	        out.println("\nITERATOR");
+	        out.print("NPolyline : ");
 	        for (Punkt horn : nLine)
 	        {
 	        	out.print(count);
 	            out.print(horn);
 	            count++;
 	        }
+	        
 	        count = 1;
 	        out.println();
-
+	        out.print("VPolyline : ");
 	        for (Punkt horn : vLine) 
 	        {
 	        	out.print(count);
@@ -71,8 +81,80 @@ public class PolyLineTest
 	        }
 	        out.println("\n");
 	        out.println("end.");
-
+	        
+			Polylinjer p = new Polylinjer();
+			System.out.println();
+			System.out.println("------------------------------------------");
+			
+			System.out.println("VEKTOR INNEHÅLLANDE N");
+			{
+			Polylinje [] poly = new Polylinje [p.getRadomAntalLinjer()];
+			poly = p.getPolyVektorN();
+			
+			for(int i = 0; i < poly.length; i++)
+			{
+		
+				System.out.println(poly[i].toString());
+			}
+			System.out.println("------------------------------------------");
+			Polylinje kortastGul = Polylinjer.findShortestYellowPolyline(poly);
+			double kortastLangd = 0;
+			
+			if(kortastGul != null)
+			{
+				kortastLangd = kortastGul.langd();
+				System.out.println("\nKortaste gula linjen är: \n" + kortastGul +
+						"\nOch har längden: " + kortastLangd + "\n");
+			}
+			
+			}
+			System.out.println("VEKTOR INNEHÅLLANDE V");
+			{
+			Polylinje [] poly = new Polylinje [p.getRadomAntalLinjer()];
+			poly = p.getPolyVektorV();
+			
+			for(int i = 0; i < poly.length; i++)
+			{
+		
+				System.out.println(poly[i].toString());
+			}
+			System.out.println("------------------------------------------");
+			Polylinje kortastGul = Polylinjer.findShortestYellowPolyline(poly);
+			double kortastLangd = 0;
+			
+			if(kortastGul != null)
+			{
+				kortastLangd = kortastGul.langd();
+				System.out.println("\nKortaste gula linjen är: \n" + kortastGul +
+						"\nOch har längden: " + kortastLangd + "\n");
+			}
+			
+			}
+	        System.out.println("VEKTOR INNEHÅLLANDE BÅDA SORTER");
+	        {
+	        
+			
+			System.out.println("------------------------------------------");
+			
+			Polylinje [] poly = new Polylinje [p.getRadomAntalLinjer()];
+			poly = p.getPolyVektorVN();
+			
+			for(int i = 0; i < poly.length; i++)
+			{
+		
+				System.out.println(poly[i].toString());
+			}
+			System.out.println("------------------------------------------");
+			Polylinje kortastGul = Polylinjer.findShortestYellowPolyline(poly);
+			double kortastLangd = 0;
+			
+			if(kortastGul != null)
+			{
+				kortastLangd = kortastGul.langd();
+				System.out.println("\nKortaste gula linjen är: \n" + kortastGul +
+						"\nOch har längden: " + kortastLangd + "\n");
+			}
+	        }
 	    }
+			
 	}
-
-
